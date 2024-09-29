d18263ac72f5424b5bb346b15964eca30935a2da
ostia27 <68474225+ostia27@users.noreply.github.com>
2024-09-29 21:33:46 +0300

    Calculator

---
+++ calculator.py
@@ -0,0 +1,21 @@
+class Calculator:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def subtract(a, b):
+        return a - b
+
+    @staticmethod
+    def multiply(a, b):
+        return a * b
+
+    @staticmethod
+    def divide(a, b):
+        if b == 0:
+            raise ZeroDivisionError("Cannot divide by zero")
+        return a / b
---
+++ test_calculator.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from calculator import Calculator
+
+
+class TestCalculator(TestCase):
+    def test_add(self):
+        self.assertEqual(Calculator.add(2, 3), 5)
+        self.assertEqual(Calculator.add(6, 9), 15)
+        self.assertEqual(Calculator.add(79, 31), 110)
+
+    def test_subtract(self):
+        self.assertEqual(Calculator.subtract(0, 0), 0)
+        self.assertEqual(Calculator.subtract(6, 9), -3)
+        self.assertEqual(Calculator.subtract(-6, -10), 4)
+
+    def test_multiply(self):
+        self.assertEqual(Calculator.multiply(2, 9), 18)
+        self.assertEqual(Calculator.multiply(0, 0), 0)
+
+    def test_divide(self):
+        self.assertEqual(Calculator.divide(15, 3), 5)
+        with self.assertRaises(ZeroDivisionError):
+            Calculator.divide(15, 0)
